75fc9008d557d9d199a65a21102451a960c00fa8
hozifakhussouna <hdhyfhkhaldhswkhh@gmail.com>
2024-04-20 23:26:44 +0200

    last2

--- lib/ForgetPasswod/ForgetPasswod/controller/cubit/forgetpassword_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'forgetpassword_cubit.dart';
-
-@immutable
-sealed class ForgetpasswordState {}
-
-final class ForgetpasswordInitial extends ForgetpasswordState {
-  
-}
--- lib/login/login/controller/cubit/login_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'login_cubit.dart';
-
-@immutable
-sealed class LoginState {}
-
-final class LoginInitial extends LoginState {}
